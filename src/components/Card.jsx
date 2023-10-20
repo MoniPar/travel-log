541434d3aa4eef1abbc46f4c10c7431d8fa77c8c
Monique <monique.parnis@gmail.com>
2023-10-20 19:21:40 +0100

    Add Card elements and styles for mobile

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,30 @@
-const Card = () => {}
+import vienna from '../assets/stephans-cathedral.png';
+
+const Card = () => {
+    return (
+        <section className="card">
+            <div className="card--img-container">
+                <img src={vienna} alt="St.Stephen's Cathedral" 
+                    className="card--img"/>
+            </div>
+            <div className="card--content">
+                <div className="card--header">
+                    <div className="location">
+                        <i className="fa-solid fa-location-dot"></i>
+                        <h2>Austria</h2>
+                    </div>
+                    <a href="#" target="_blank">View on Google Maps</a>
+                </div>
+                <h3>Vienna</h3>
+                <h4>2nd - 3rd August 2023</h4>
+                <p>Baroque street-scapes and imperial palaces set the stage 
+                    for Vienna's artistic and musical masterpieces alongside 
+                    its coffee-house culture and vibrant epicurean and design scenes.
+                </p>
+            </div>
+
+        </section>
+    )
+}
 
 export default Card;
